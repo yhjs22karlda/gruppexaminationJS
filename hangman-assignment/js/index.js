6483f5c232876282c77b4b793fd4b3938a3f61b4
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2022-10-31 08:46:51 +0100

    Added code

---
+++ hangman-assignment/js/index.js
@@ -0,0 +1,9 @@
+/**
+ För att toggla SVG:en
+ document.querySelector('figure').classList.add('scaffold')
+ document.querySelector('figure').classList.add('head')
+ document.querySelector('figure').classList.add('body')
+ document.querySelector('figure').classList.add('arms')
+ document.querySelector('figure').classList.add('legs')
+
+ */
